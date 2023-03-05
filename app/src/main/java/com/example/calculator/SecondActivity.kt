90e647647cb50da1efde24f7e88fbeee6ab5b982
Danil <shvydkodan@gmail.com>
2023-03-05 14:45:16 +0200

    Square UI Added

---
+++ app/src/main/java/com/example/calculator/SecondActivity.kt
@@ -0,0 +1,21 @@
+package com.example.calculator
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class SecondActivity : AppCompatActivity() {
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+
+
+
+    }
+}
