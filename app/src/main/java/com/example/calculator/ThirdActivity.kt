6c3595d503e4acbb81cd82c07deda80f354b2cba
Danil <shvydkodan@gmail.com>
2023-03-05 18:50:25 +0200

    Rectangle UI + Program Completed

--- app/src/main/java/com/example/calculator/ThirdActivity.kt
+++ app/src/main/java/com/example/calculator/ThirdActivity.kt
@@ -44,5 +44,33 @@ class ThirdActivity : AppCompatActivity() {
             true
         }
 
+        user_field_a = findViewById(R.id.rectangle_a_side_input)
+        user_field_b = findViewById(R.id.rectangle_b_side_input)
+        main_btn = findViewById(R.id.rectangle_button_calc)
+        result_field = findViewById(R.id.rectangle_result)
+
+        main_btn?.setOnClickListener {
+            if (user_field_a?.text?.toString()?.trim()?.equals("")!!)
+                Toast.makeText(this, "Введіть сторону a", Toast.LENGTH_SHORT).show()
+            else {
+
+
+
+                if (user_field_b?.text?.toString()?.trim()?.equals("")!!)
+                    Toast.makeText(this, "Введіть сторону b", Toast.LENGTH_SHORT).show()
+                else {
+                    val a0 = user_field_a?.text.toString()
+                    val a = a0.toDouble()
+                    val b0 = user_field_b?.text.toString()
+                    val b = b0.toDouble()
+
+                    val s = a * b
+                    result_field?.text = "S = $s"
+
+                }
+            }
+
+
+        }
     }
 }
